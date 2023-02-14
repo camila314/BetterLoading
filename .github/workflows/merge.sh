ae540e977a32a971972f194850bb46926b33e597
camila314 <47485054+camila314@users.noreply.github.com>
2023-02-14 10:42:01 -0600

    actions issue

--- .github/workflows/merge.sh
+++ .github/workflows/merge.sh
@@ -1,9 +1,9 @@
 mod_name=BetterLoading
 args=""
-if [ -f "built/$mod_name/Release/$mod_name.dll" ]; then
+if [ -f "built/Release/$mod_name.dll" ]; then
 	args="$args --binary built/$mod_name/Release/$mod_name.dll"
 fi
-if [ -f "built/$mod_name/lib$mod_name.dylib" ]; then
+if [ -f "built/lib$mod_name.dylib" ]; then
 	args="$args --binary built/$mod_name/lib$mod_name.dylib"
 fi
 ./geode.exe package new $mod_name $args --output $mod_name.geode
